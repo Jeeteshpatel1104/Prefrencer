d95fba49d6b1cd3be108d97ac29e2d68b01ef44c
Jeetesh Patel <jeeteshpatel1104@gmail.com>
2025-08-14 01:49:18 +0530

    iski

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ pool.connect()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'http://localhost:3000/auth/google/callback' 
+    callbackURL:'https://prefrencer.onrender.com/auth/google/callback' 
     // callbackURL:'https://prefrencer.onrender.com/auth/google/callback' 
         	// http://localhost:3000/auth/google/callback
 }, async (accessToken, refreshToken, profile, done) => {
